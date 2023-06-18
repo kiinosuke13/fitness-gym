87fa343c088d91039e8ac78ee723263fa5162196
きい <89005440+kiinosuke13@users.noreply.github.com>
2023-06-18 09:40:11 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 jQuery('.slick01').slick({
     infinite: true,  
     arrows: false,//左右の矢印はなし
-    autoplay: false,//自動的に動き出すか。初期値はfalse。
+    autoplay: true,//自動的に動き出すか。初期値はfalse。
     autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
     speed: 6900,//スライドのスピード。初期値は300。
     infinite: true,//スライドをループさせるかどうか。初期値はtrue。
@@ -22,7 +22,7 @@ jQuery('.slick01').slick({
 ]});
 
 jQuery('.slick02').slick({
-    infinite: true,  
+    infinite: true,
     arrows: true,//左右の矢印はなし
     autoplay: false,//自動的に動き出すか。初期値はfalse。
     autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
@@ -32,11 +32,13 @@ jQuery('.slick02').slick({
     pauseOnFocus: false,//フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
     cssEase: 'linear',//動き方。初期値はeaseですが、スムースな動きで見せたいのでlinear
     slidesToScroll: 1,//1回のスライドで動かす要素数
+    variableWidth:false,
         responsive: [
       {
       breakpoint: 767,//モニターの横幅が769px以下の見せ方
       settings: {
         slidesToShow: 1,//スライドを画面に2枚見せる
+
       }
     },
 ]});
